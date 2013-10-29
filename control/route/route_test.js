c781cf25125196af924282c48ec6874a5b9d30f1
Josh Dean <imjoshdean@me.com>
2013-10-29 10:10:36 -0500

    Add breaking test in relation to #359

--- control/route/route_test.js
+++ control/route/route_test.js
@@ -81,5 +81,52 @@ test("dont overwrite defaults (#474)", function(){
 	
 })
 
+test("multiple controls on the same route (#359)", function() {
+	expect(2);
+	
+	var Files = can.Control.extend({
+		"test/:type route": function(params) {
+			equal(params.type, "files", 'hit route event on Files control');
+		}
+	});
+
+	var Contacts = can.Control.extend({
+		"test/:type route": function(params) {
+			equal(params.type, "contacts", 'hit route event on Contacts control');
+		}
+	});
+
+	var views = {
+	    contacts: Contacts,
+	    files: Files
+	};
+
+	var ParentControl = can.Control.extend({
+		"test/:type route": function(params) {
+			var control = can.$('#child').data('controls');
+			if(control) {
+				control[0].destroy();
+			}
+			new views[params.type]('#child');
+		}
+	});
+
+	var parent = document.createElement('div'),
+		child = document.createElement('div');
+	parent.id = 'parent';
+	child.id = 'child';
+	can.$('#qunit-test-area').append(parent);
+	can.$('#qunit-test-area').append(child);
+
+	new ParentControl('#parent');
+	can.route.ready();
+
+	window.location.hash = "#!test/files";
+	can.trigger(window, 'hashchange');
+
+	window.location.hash = "#!test/contacts";
+	can.trigger(window, 'hashchange');
+})
+
 
 })();
